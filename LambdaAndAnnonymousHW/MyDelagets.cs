101c71b425ec45e947bfb2570fd437022a8b30ba
user <user@DESKTOP-RVDE87M>
2021-10-16 16:34:00 +0300

    Add project files.

---
+++ LambdaAndAnnonymousHW/MyDelagets.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LambdaAndAnnonymousHW
+{
+    delegate void ShalomDelagete();
+    delegate void Greet(string name);
+    delegate void CalculationDel(int num1,int num2);
+}
---
+++ LambdaAndAnnonymousHW/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaAndAnnonymousHW
+{
+    class Program
+    { 
+        
+        static void Main(string[] args)
+        {
+            #region Annonymous
+            
+            ShalomDelagete delagete = delegate () { Console.WriteLine("Shalom"); };
+            Greet greet = delegate (string nm) { Console.WriteLine($"Hello {nm}"); };
+            CalculationDel caldl = delegate (int a, int b) { Console.WriteLine(a + b); };  
+            
+            greet("or");
+            delagete();
+            #endregion
+
+            #region Lambda
+            string name = "or";
+;            ShalomDelagete shalom = () => { Console.WriteLine("Hello"); };
+             Greet greet1 = (string ng) => { Console.WriteLine($"Hello {ng}"); };
+            CalculationDel del = (int a, int b) => { Console.WriteLine(a + b); };
+            greet1(name);
+            del(1, 2);
+
+            #endregion
+        }
+    }
+}
